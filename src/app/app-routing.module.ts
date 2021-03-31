5cfd6f67158325962a8d6f64af0b45fe2cf41b43
glassesUSA <theo.f@glassesusa.com>
2021-03-31 13:29:17 +0300

    Updates!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-
-const routes: Routes = [];
-=======
 import { CtaButtonComponent } from './pages/components/cta-button/cta-button.component';
 import { GettingStartedComponent } from './pages/getting-started/getting-started.component';
 import { IntroductionComponent } from './pages/introduction/introduction.component';
@@ -13,7 +9,6 @@ const routes: Routes = [
   { path: 'getting-started', component: GettingStartedComponent },
   { path: 'components', component: CtaButtonComponent }
 ];
->>>>>>> 3dacc5adeaf7067cd9038cb68d86c54a7a56f6c0
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
